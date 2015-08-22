071435d27833877c9b2118bdcef2da0cc27d0254
Alan Cecil <alan.cecil@gmail.com>
2015-08-22 13:06:46 -0700

    Angular test comence

---
+++ app.js
@@ -0,0 +1,22 @@
+var arcomApp = angular.module('arcomApp', ['ngRoute'])
+	.config(['$routeProvider',function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainController',
+				controllerAs: 'main'
+			})
+			.when('/science', {
+				templateUrl: 'views/science.html',
+				controller: 'ScienceController',
+				controllerAs: 'science'
+			})
+			.when('/wow', {
+				templateUrl: 'views/wow.html',
+				controller: 'WowController',
+				controllerAs: 'wow'
+			})
+			.otherwiser({
+				redirecTo: '/'
+			});
+	}])
