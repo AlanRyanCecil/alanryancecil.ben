83d3b38c7c386a47f2d8ef58e0d699f564f8b728
Alan Cecil <alan.cecil@gmail.com>
2016-03-17 04:00:05 -0700

    origin only

--- xxxserver.js
+++ xxxserver.js
@@ -8,4 +8,4 @@ var port = 8000;
 app.use(express.static(path));
 app.listen(port);
 
-console.log('alandotApp running on port: ' + port + '   NODEMON. ');
+console.log('alandotApp running on port: ' + port + '   NODEMON farts. ');
